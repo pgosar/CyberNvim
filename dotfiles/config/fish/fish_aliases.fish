8b774d6f4050d1cb6d10128cfd48459b311b9fdb
pgosar <pgosarsteam@gmail.com>
2022-05-13 17:43:40 -0500

    fish aliases and commands

---
+++ dotfiles/config/fish/fish_aliases.fish
@@ -0,0 +1,30 @@
+# random
+alias update="sudo apt update -y && sudo apt upgrade -y"
+alias ls="ls -F --color=auto --group-directories-first"
+alias sl="ls -F --color=auto --group-directories-first"
+alias lsa="ls -a"
+alias grep="grep --color=auto"
+alias ccat="pygmentize -g"
+alias batch="cmd.exe /c"
+alias py="python3"
+alias v="vim"
+alias dora="explorer.exe ."
+
+# git stuff
+alias gcom="git commit -a"
+alias log="git log --graph --date=relative --pretty=tformat:'%Cred%h%Creset -%C(auto) %d%Creset %s %Cgreen(%an %ad)%Creset'"
+alias gst="git status"
+alias amend="git commit --amend --no-edit"
+alias reword="git commit --amend"
+alias untrack="rm --cache"
+alias uncommit="reset --soft HEAD~1"
+
+# verbosity and prompting
+alias mkdir="mkdir -pv"
+alias rm="rm -iv"
+alias cp="cp -iv"
+alias mv="mv -iv"
+
+# packages
+alias hh="hstr"
+alias yt="youtube-dl --add-metadata -ic"
