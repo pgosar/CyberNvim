9b94e7bba31e870f4277c60a1cea510a5bf0076c
pgosar <pgosarsteam@gmail.com>
2023-01-19 03:14:09 -0600

    update

--- dotfiles/config/shells/fish/config.fish
+++ dotfiles/config/shells/fish/config.fish
@@ -11,12 +11,6 @@
 #
 #
 
-if status is-interactive
-    set CONFIG $HOME/code/useful-things/dotfiles/config/shells/fish
-    source $CONFIG/fish_aliases.fish
-    source $CONFIG/fish_commands.fish
-    sudo hwclock -s
-  if status is-interactive and not set -q TMUX
-    tmux new-session -A -s main
-  end
-end
+set CONFIG $HOME/code/useful-things/dotfiles/config/shells/fish
+source $CONFIG/fish_aliases.fish
+source $CONFIG/fish_commands.fish
