11c7bc14e8bfe1fb85313c1f336e7b6e2ff3d4d7
pgosar <pgosarsteam@gmail.com>
2023-02-14 13:19:38 -0600

    fix deprecated script

--- scripts/python/pdfMerge.py
+++ scripts/python/pdfMerge.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
 import os
-from PyPDF2 import PdfFileMerger
+from PyPDF2 import PdfMerger
 
 pdfs = [pdf for pdf in os.listdir() if pdf.endswith(".pdf")]
-merger = PdfFileMerger()
+merger = PdfMerger()
 print("pdf name: ")
 filename = input()
 
 for pdf in pdfs:
-    merger.append(open(pdf, "rb"))
+    merger.append(pdf)
 
-with open(filename + ".pdf", "wb") as fout:
-    merger.write(fout)
+merger.write(filename)
+merger.close()
