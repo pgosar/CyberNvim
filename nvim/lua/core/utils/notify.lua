fc821fe8c7fd8994bc74aecea135865d410250a1
pgosar <pgosarsteam@gmail.com>
2023-06-05 18:17:30 -0500

    make notifications functional (plugins online borked)

--- nvim/lua/core/utils/notify.lua
+++ nvim/lua/core/utils/notify.lua
@@ -1,3 +1,4 @@
+vim.notify = require("notify")
 local client_notifs = {}
 
 local function get_notif_data(client_id, token)
@@ -37,7 +38,6 @@ local function format_message(message, percentage)
     return (percentage and percentage .. "%\t" or "") .. (message or "")
 end
 
--- table from lsp severity to vim severity.
 local severity = {
     "error", "warn", "info", "info" -- map both hint and info to info?
 }
@@ -51,7 +51,7 @@ dap.listeners.before['event_progressStart']['progress-notifications'] =
         local notif_data = get_notif_data("dap", body.progressId)
 
         local message = format_message(body.message, body.percentage)
-        notif_data.notification = vim.notify(message, 1, {
+        notif_data.notification = vim.notify(message, "info", {
             title = format_title(body.title, session.config.type),
             icon = spinner_frames[1],
             timeout = false,
@@ -67,7 +67,7 @@ dap.listeners.before['event_progressUpdate']['progress-notifications'] =
         local notif_data = get_notif_data("dap", body.progressId)
         notif_data.notification = vim.notify(
                                       format_message(body.message,
-                                                     body.percentage), 1, {
+                                                     body.percentage), "info", {
                 replace = notif_data.notification,
                 hide_from_history = false
             })
@@ -78,7 +78,7 @@ dap.listeners.before['event_progressEnd']['progress-notifications'] = function(
     local notif_data = client_notifs["dap"][body.progressId]
     notif_data.notification = vim.notify(body.message and
                                              format_message(body.message) or
-                                             "Complete", 1, {
+                                             "Complete", "info", {
         icon = "",
         replace = notif_data.notification,
         timeout = 3000
@@ -86,23 +86,107 @@ dap.listeners.before['event_progressEnd']['progress-notifications'] = function(
     notif_data.spinner = nil
 end
 
-local function _rename()
-    local pos = vim.lsp.util.make_position_params()
-    pos.oldName = vim.fn.expand("<cword>")
-    vim.ui.input({prompt = 'rename to> ', default = pos.oldName},
-                 function(input)
-        if input == nil then return end
+vim.lsp.handlers["$/progress"] = function(_, result, ctx)
+ local client_id = ctx.client_id
 
-        pos.newName = input
-        vim.lsp.buf_request(0, "textDocument/rename", pos,
-                            function(err, res, ctx, config)
-            vim.lsp.handlers["textDocument/rename"](err, res, ctx, config)
+ local val = result.value
+
+ if not val.kind then
+   return
+ end
+ local notif_data = get_notif_data(client_id, result.token)
+if not notif_data then return end
+ if val.kind == "begin" then
+   local message = format_message(val.message, val.percentage)
+
+   notif_data.notification = vim.notify(message, "info", {
+     title = format_title(val.title, vim.lsp.get_client_by_id(client_id).name),
+     icon = spinner_frames[1],
+     timeout = false,
+     hide_from_history = false,
+   })
+
+   notif_data.spinner = 1
+   update_spinner(client_id, result.token)
+ elseif val.kind == "report" and notif_data then
+   notif_data.notification = vim.notify(format_message(val.message, val.percentage), "info", {
+     replace = notif_data.notification,
+     hide_from_history = false,
+   })
+ elseif val.kind == "end" and notif_data then
+   notif_data.notification =
+     vim.notify(val.message and format_message(val.message) or "Complete", "info", {
+       icon = "",
+       replace = notif_data.notification,
+       timeout = 3000,
+     })
+
+   notif_data.spinner = nil
+ end
+end
+
+local function _rename()
+    local param = vim.lsp.util.make_position_params()
+    param.oldName = vim.fn.expand("<cword>")
+    vim.ui.input({prompt = 'rename to> ', default = param.oldName},
+                 function(input)
+        if input == nil then
+            vim.notify('aborted', "warn",
+                         {title = '[LSP] rename', render = 'compact'})
+            return
+        end
+        param.newName = input
+        vim.lsp.buf_request(0, "textDocument/rename", param,
+                            function(err, result, ctx, config)
+            if not result or (not result.documentChanges and not result.changes) then
+                vim.notify(string.format('could not perform rename: %s -> %s',
+                                         param.oldName,
+                                         param.newName), "error",
+                           {title = '[LSP] rename', timeout = 500})
+
+                return
+            end
+
+            vim.lsp.handlers["textDocument/rename"](err, result, ctx, config)
 
             local notif, entries = {}, {}
-            local updates = 0
+            local files, updates = 0, 0
 
-            if res.changes then
-                for uri, edits in pairs(res.changes) do
+            if result.documentChanges then
+                for _, document in pairs(result.documentChanges) do
+                    files = files + 1
+                    local uri = document.textDocument.uri
+                    local bufnr = vim.uri_to_bufnr(uri)
+
+                    for _, edit in ipairs(document.edits) do
+                        local start_line = edit.range.start.line + 1
+                        local line = vim.api.nvim_buf_get_lines(bufnr,
+                                                                start_line - 1,
+                                                                start_line,
+                                                                false)[1]
+
+                        table.insert(entries, {
+                            bufnr = bufnr,
+                            lnum = start_line,
+                            col = edit.range.start.character + 1,
+                            text = line
+                        })
+                    end
+
+                    updates = updates + vim.tbl_count(document.edits)
+
+                    local short_uri = string.sub(vim.uri_to_fname(uri),
+                                                 #vim.loop.cwd() + 2)
+                    table.insert(notif, string.format('\t- %d in %s',
+                                                             vim.tbl_count(
+                                                                 document.edits),
+                                                             short_uri))
+                end
+            end
+
+            if result.changes then
+                for uri, edits in pairs(result.changes) do
+                    files = files + 1
                     local bufnr = vim.uri_to_bufnr(uri)
 
                     for _, edit in ipairs(edits) do
@@ -111,6 +195,7 @@ local function _rename()
                                                                 start_line - 1,
                                                                 start_line,
                                                                 false)[1]
+
                         table.insert(entries, {
                             bufnr = bufnr,
                             lnum = start_line,
@@ -118,30 +203,44 @@ local function _rename()
                             text = line
                         })
                     end
+
                     updates = updates + vim.tbl_count(edits)
+
                     local short_uri = string.sub(vim.uri_to_fname(uri),
                                                  #vim.loop.cwd() + 2)
                     table.insert(notif, string.format('\t- %d in %s',
-                                                      vim.tbl_count(edits),
-                                                      short_uri))
+                                                             vim.tbl_count(edits),
+                                                             short_uri))
                 end
             end
-            local str = ''
-            str = string.format('made %s', notif[1]:sub(4))
-            local loc = str:find('in')
 
-            str = table.concat({
-                str:sub(1, loc - 1),
-                string.format('change%s ', (updates > 1 and "s") or ""),
-                str:sub(loc)
-            }, '')
-            vim.notify(str, 1, {
-                title = string.format('[LSP] rename: %s -> %s', pos.oldName,
-                                      pos.newName),
+            local str = ''
+            if files > 1 then
+                table.insert(notif, "info",
+                             string.format('made %d change%s in %d files',
+                                           updates,
+                                           (updates > 1 and "s") or "",
+                                           files))
+
+                str = table.concat(notif, '\n')
+            else
+                str = string.format('made %s',
+                                                 notif[1]:sub(4))
+
+                local insert_loc = str:find('in')
+                str = table.concat({
+                    str:sub(1, insert_loc - 1),
+                    string.format('change%s ', (updates > 1 and "s") or ""),
+                    str:sub(insert_loc)
+                }, '')
+            end
+            vim.notify(str, "info", {
+                title = string.format('[LSP] rename: %s -> %s',
+                                      param.oldName,
+                                      param.newName),
                 timeout = 2500
             })
         end)
     end)
 end
-
 vim.lsp.buf.rename = _rename
