4af07bc2e116f6920c904e5968f80ad1dc60a7b1
pgosar <pgosarsteam@gmail.com>
2023-06-07 05:42:41 -0500

    remember folds

--- nvim/lua/core/autocommands.lua
+++ nvim/lua/core/autocommands.lua
@@ -3,30 +3,35 @@
 local augroup = vim.api.nvim_create_augroup
 local cmd = vim.api.nvim_create_autocmd
 
-cmd({ 'BufWinLeave', 'BufWritePost' }, {
-  desc = "auto run :PackerCompile whenever plugins.lua is updated",
-  group = augroup("packer", { clear = true }),
-  pattern = "plugins.lua",
-  command = "source <afile> | PackerCompile",
+cmd({ "BufWinLeave", "BufWritePost" }, {
+	desc = "auto run :PackerCompile whenever plugins.lua is updated",
+	group = augroup("packer", { clear = true }),
+	pattern = "plugins.lua",
+	command = "source <afile> | PackerCompile",
 })
 
-cmd({ 'BufEnter', 'BufAdd', 'BufNew', 'BufNewFile', 'BufWinEnter' }, {
-  desc = "fix tree sitter folds issue",
-  group = augroup('treesitter folds', { clear = true }),
-  pattern = "*",
-  callback = function()
-    vim.opt.foldmethod = 'expr'
-    vim.opt.foldexpr   = 'nvim_treesitter#foldexpr()'
-  end,
+cmd({ "BufEnter", "BufAdd", "BufNew", "BufNewFile", "BufWinEnter" }, {
+	desc = "fix tree sitter folds issue",
+	group = augroup("treesitter folds", { clear = true }),
+	pattern = "*",
+	callback = function()
+		vim.opt.foldmethod = "expr"
+		vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+	end,
 })
 
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  pattern = { "*" },
-  command = [[%s/\s\+$//e]],
+	pattern = { "*" },
+	command = [[%s/\s\+$//e]],
 })
-
 
 vim.cmd([[autocmd BufReadPost *
   \ if line("'\"") >= 1 && line("'\"") <= line("$") |
   \   exe "normal! g`\"" |
   \ endif]])
+
+vim.cmd([[augroup remember_folds
+  autocmd!
+  autocmd BufWinLeave * mkview
+  autocmd BufWinEnter * silent! loadview
+augroup END]])
