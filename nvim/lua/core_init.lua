445b58171738585e5ae8db8f9b042a74dae5878c
pgosar <pgosarsteam@gmail.com>
2023-06-07 05:29:48 -0500

    clean old lsp config

--- nvim/lua/core_init.lua
+++ nvim/lua/core_init.lua
@@ -1,8 +1,14 @@
-for _, source in ipairs {
-  "core.utils.utils", "core.autocommands", "core.main-options",
-  "core.plugins", "core.keybindings", "core.utils.notify"
+-- luacheck: globals vim
 
-} do
+for _, source in ipairs({
+  "core.utils.utils",
+  "core.autocommands",
+  "core.main-options",
+  "core.plugins",
+  "core.lsp",
+  "core.keybindings",
+  "core.utils.notify",
+}) do
   local status_ok, fault = pcall(require, source)
   if not status_ok then
     vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault)
@@ -10,18 +16,14 @@ for _, source in ipairs {
 end
 
 require("neodev").setup()
-require('Comment').setup()
-local lsp = require('lsp-zero')
-lsp.preset('recommended')
-lsp.setup()
+require("Comment").setup()
 
-require('plugin-configs.null-ls')
-
-require('bufferline').setup {}
-require('scope').setup {}
+require("bufferline").setup({})
+require("scope").setup({})
 
 vim.cmd([[ colorscheme onedark ]])
 
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
 vim.notify = require("notify")
+
