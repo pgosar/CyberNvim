4fe8381694f78399c87c14dbb3e5e9be01d0b099
pgosar <pgosarsteam@gmail.com>
2023-07-27 04:47:36 -0500

    fix(ui): move cmdline and popup to the bottom

--- lua/plugin-configs/noice.lua
+++ lua/plugin-configs/noice.lua
@@ -9,6 +9,13 @@ noice.setup({
 		{
 			filter = {
 				event = "msg_show",
+				kind = "search_count",
+			},
+			opts = { skip = true },
+		},
+		{
+			filter = {
+				event = "msg_show",
 				kind = "",
 				find = "written",
 			},
@@ -24,7 +31,22 @@ noice.setup({
 		},
 	},
 	cmdline = { view = "cmdline" },
+	views = {
+		popupmenu = {
+			size = { width = 50, height = 10 },
+			border = {
+				style = "rounded",
+				padding = { 0, 1 },
+			},
+			win_options = {
+				winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+			},
+		},
+	},
 	lsp = {
+		message = {
+			enabled = false,
+		},
 		override = {
 			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 			["vim.lsp.util.stylize_markdown"] = true,
@@ -33,7 +55,6 @@ noice.setup({
 	},
 	presets = {
 		bottom_search = true,
-		command_palette = true,
 		long_message_to_split = true,
 		lsp_doc_border = true,
 	},
