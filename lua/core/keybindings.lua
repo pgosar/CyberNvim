e1709c92eeee64818b766e8950bf8db94966b80b
pgosar <pgosarsteam@gmail.com>
2023-07-30 19:46:33 -0500

    refactor(keybinds): move gitsigns keybinds to keybinds file

--- lua/core/keybindings.lua
+++ lua/core/keybindings.lua
@@ -98,5 +98,59 @@ map("n", "<leader>bt", "<CMD>lua term.bashtop_toggle()<CR>")
 -- Hop
 map("n", "<leader>j", "<CMD>HopWord<CR>")
 
+-- Gitsigns
+
+-- making this a function here because all it does is create keybinds for gitsigns but it needs
+-- to be attached to an on_attach function.
+local function _gitsigns()
+	local gs = package.loaded.gitsigns
+	local map = require("core.utils.utils").map
+	map("n", "]c", function()
+		if vim.wo.diff then
+			return "]c"
+		end
+		vim.schedule(function()
+			gs.next_hunk()
+		end)
+		return "<Ignore>"
+	end, { expr = true })
+	map("n", "[c", function()
+		if vim.wo.diff then
+			return "[c"
+		end
+		vim.schedule(function()
+			gs.prev_hunk()
+		end)
+		return "<Ignore>"
+	end, { expr = true })
+
+	map("n", "<leader>hs", gs.stage_hunk)
+	map("n", "<leader>hr", gs.reset_hunk)
+	map("v", "<leader>hs", function()
+		gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+	end)
+	map("v", "<leader>hr", function()
+		gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+	end)
+	map("n", "<leader>hS", gs.stage_buffer)
+	map("n", "<leader>hu", gs.undo_stage_hunk)
+	map("n", "<leader>hR", gs.reset_buffer)
+	map("n", "<leader>hp", gs.preview_hunk)
+	map("n", "<leader>hb", function()
+		gs.blame_line({ full = true })
+	end)
+	map("n", "<leader>tb", gs.toggle_current_line_blame)
+	map("n", "<leader>hd", gs.diffthis)
+	map("n", "<leader>hD", function()
+		gs.diffthis("~")
+	end)
+	map("n", "<leader>td", gs.toggle_deleted)
+	map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+end
+
 -- Misc
 map("n", "<leader>as", "<CMD>ASToggle<CR>")
+
+return {
+	gitsigns = _gitsigns,
+}
