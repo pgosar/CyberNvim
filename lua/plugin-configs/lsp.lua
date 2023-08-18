1a6876d058ac4bbd4b11554b9a520d70858e5828
pgosar <pgosarsteam@gmail.com>
2023-08-18 14:24:19 -0500

    chore: bump

--- lua/plugin-configs/lsp.lua
+++ lua/plugin-configs/lsp.lua
@@ -2,32 +2,31 @@ local lsp = require("lsp-zero")
 lsp.preset("minimal")
 
 lsp.set_sign_icons({
-  error = "✘",
-  warn = "▲",
-  hint = "⚑",
-  info = "»",
+	error = "✘",
+	warn = "▲",
+	hint = "⚑",
+	info = "»",
 })
 
 lsp.set_server_config({
-  capabilities = {
-    textDocument = {
-      foldingRange = {
-        dynamicRegistration = false,
-        lineFoldingOnly = true,
-      },
-    },
-  },
+	capabilities = {
+		textDocument = {
+			foldingRange = {
+				dynamicRegistration = false,
+				lineFoldingOnly = true,
+			},
+		},
+	},
 })
 
 local group = require("user.user_config").enable_plugins
 if not require("core.utils.utils").enabled(group, "autosave") then
-  lsp.format_on_save({
-    format_opts = {
-      async = false,
-      timeout_ms = 10000,
-    },
-    servers = require("user.user_config").formatting_servers,
-  })
+	lsp.format_on_save({
+		format_opts = {
+			async = false,
+			timeout_ms = 10000,
+		},
+		servers = require("user.user_config").formatting_servers,
+	})
 end
-
 lsp.setup()
--- lua/plugin-configs/null-ls.lua
+++ lua/plugin-configs/null-ls.lua
@@ -2,5 +2,5 @@ local null_ls = require("null-ls")
 local conf = require("user.user_config")
 local sources = conf.setup_sources and conf.setup_sources(null_ls.builtins) or nil
 null_ls.setup({
-  sources = sources,
+	sources = sources,
 })
