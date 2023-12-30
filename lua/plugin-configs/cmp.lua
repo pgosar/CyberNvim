0af6deb92178a5577158ff1c4867d8e917b4c9e7
pgosar <pgosarsteam@gmail.com>
2023-12-29 22:38:26 -0600

    fix: cmp completions

--- lua/plugin-configs/cmp.lua
+++ lua/plugin-configs/cmp.lua
@@ -64,6 +64,7 @@ cmp.setup({
 		["<C-k>"] = cmp.mapping.scroll_docs(-4),
 		["<C-j>"] = cmp.mapping.scroll_docs(4),
 		["<C-c>"] = cmp.mapping.abort(),
+		["<C-n>"] = { i = cmp.mapping.complete() },
 		["<C-f>"] = cmp_action.luasnip_jump_forward(),
 		["<C-b>"] = cmp_action.luasnip_jump_backward(),
 	},
