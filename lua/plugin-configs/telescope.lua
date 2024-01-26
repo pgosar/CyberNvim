ba5b7eba1515dcd9a87149a92f404a91244f07cd
pgosar <pgosarsteam@gmail.com>
2024-01-26 15:57:55 -0600

    refactor(plugins): remove unnecessary binds

--- lua/plugin-configs/telescope.lua
+++ lua/plugin-configs/telescope.lua
@@ -1,19 +1,6 @@
-local trouble = require("trouble.providers.telescope")
 local telescope = require("telescope")
 telescope.load_extension("scope")
 telescope.load_extension("aerial")
 telescope.load_extension("notify")
 telescope.load_extension("noice")
 telescope.load_extension("projects")
-
-telescope.setup({
-	defaults = {
-		mappings = {
-			i = {
-				["<c-t>"] = trouble.open_with_trouble,
-				["<C-h>"] = "which_key",
-			},
-			n = { ["<c-t>"] = trouble.open_with_trouble },
-		},
-	},
-})
