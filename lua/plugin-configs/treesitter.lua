4add4ea9fbd561b7e92ccd2e595a872399d2a082
pgosar <pgosarsteam@gmail.com>
2024-05-26 15:36:13 -0500

    fix(plugins): html highlighting issues

--- lua/plugin-configs/treesitter.lua
+++ lua/plugin-configs/treesitter.lua
@@ -1,14 +1,10 @@
 require("nvim-treesitter.configs").setup({
 	highlight = {
-		disable = { "html" },
+		enable = true,
 	},
 	incremental_selection = { enable = true },
 	autotag = { enable = true },
 	rainbow = { enable = true, disable = { "html" }, extended_mode = false },
-})
-
-require("ts_context_commentstring").setup({
-	enable_autocmd = false,
 })
 
 require("treesitter-context").setup({
