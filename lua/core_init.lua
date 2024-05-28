c567e038a94387798631b8f58e0c48949009929c
pgosar <pgosarsteam@gmail.com>
2024-05-28 01:47:02 -0500

    fix(lsp): reload lsp-zero just in case

--- lua/core_init.lua
+++ lua/core_init.lua
@@ -47,6 +47,8 @@ if enabled(group, "treesitter") then
 	end
 end
 
+pcall(require, "lsp-zero")
+
 if exist and type(user_config) == "table" and user_config.user_conf then
 	user_config.user_conf()
 end
--- lua/plugin-configs/null-ls.lua
+++ lua/plugin-configs/null-ls.lua
@@ -1,7 +1,11 @@
 local null_ls = require("null-ls")
 
 local exist, user_config = pcall(require, "user.user_config")
-local sources = exist and type(user_config) == "table" and user_config.setup_sources and user_config.setup_sources(null_ls.builtins) or {}
+local sources = exist
+		and type(user_config) == "table"
+		and user_config.setup_sources
+		and user_config.setup_sources(null_ls.builtins)
+	or {}
 
 null_ls.setup({
 	sources = sources,
