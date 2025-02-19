bb3e352c3c1734dc13e243f084a6c4c1bee9d6e5
Aaron <2738518+NeonArray@users.noreply.github.com>
2025-02-19 13:01:47 -0500

    Update example_user_config.lua
    Add explanation as well as a link to the repository for each plugin

--- lua/example_user_config.lua
+++ lua/example_user_config.lua
@@ -57,44 +57,157 @@ M.autocommands = {
 -- set to false to disable plugins
 -- Default value is true if left blank
 M.enable_plugins = {
-	aerial = true,
-	alpha = true,
-	autotag = true,
-	bufferline = true,
-	context = true,
-	copilot = true,
-	dressing = true,
-	gitsigns = true,
-	hop = true,
-	img_clip = true,
-	indent_blankline = true,
-	lsp_zero = true,
-	lualine = true,
-	neodev = true,
-	neoscroll = true,
-	neotree = true,
-	session_manager = true,
-	noice = true,
-	null_ls = true,
-	autopairs = true,
-	cmp = true,
-	colorizer = true,
-	dap = true,
-	notify = true,
-	surround = true,
-	treesitter = true,
-	ufo = true,
-	onedark = true,
-	project = true,
-	rainbow = true,
-	scope = true,
-	telescope = true,
-	toggleterm = true,
-	trouble = true,
-	twilight = true,
-	whichkey = true,
-	windline = true,
-	zen = true,
+	-- aerial: Code outline window for skimming and quick navigation
+	-- https://github.com/stevearc/aerial.nvim
+	aerial = false,
+	
+	-- alpha: Customizable start screen for Neovim
+	-- https://github.com/goolord/alpha-nvim
+	alpha = false,
+	
+	-- autotag: Automatically close and rename HTML/XML tags
+	-- https://github.com/windwp/nvim-ts-autotag
+	autotag = false,
+	
+	-- bufferline: Snazzy buffer line for Neovim
+	-- https://github.com/akinsho/bufferline.nvim
+	bufferline = false,
+	
+	-- context: Shows the current function context in the command line
+	-- https://github.com/wellle/context.vim
+	context = false,
+	
+	-- copilot: AI-powered code completion and suggestion
+	-- https://github.com/github/copilot.vim
+	copilot = false,
+	
+	-- dressing: Improve the default Neovim UI
+	-- https://github.com/stevearc/dressing.nvim
+	dressing = false,
+	
+	-- gitsigns: Git signs in the sign column
+	-- https://github.com/lewis6991/gitsigns.nvim
+	gitsigns = false,
+	
+	-- hop: Easy motion-like navigation
+	-- https://github.com/phaazon/hop.nvim
+	hop = false,
+	
+	-- img_clip: Paste images from clipboard into Neovim
+	-- https://github.com/ekickx/clipboard-image.nvim
+	img_clip = false,
+	
+	-- indent_blankline: Indentation guides for Neovim
+	-- https://github.com/lukas-reineke/indent-blankline.nvim
+	indent_blankline = false,
+	
+	-- lsp_zero: Lightweight LSP configuration for Neovim
+	-- https://github.com/VonHeikemen/lsp-zero.nvim
+	lsp_zero = false,
+	
+	-- lualine: Fast and easy-to-configure statusline
+	-- https://github.com/nvim-lualine/lualine.nvim
+	lualine = false,
+	
+	-- neodev: Neovim setup for init.lua and plugin development
+	-- https://github.com/folke/neodev.nvim
+	neodev = false,
+	
+	-- neoscroll: Smooth scrolling for Neovim
+	-- https://github.com/karb94/neoscroll.nvim
+	neoscroll = false,
+	
+	-- neotree: File explorer tree for Neovim
+	-- https://github.com/nvim-neo-tree/neo-tree.nvim
+	neotree = false,
+	
+	-- session_manager: Session management for Neovim
+	-- https://github.com/Shatur/neovim-session-manager
+	session_manager = false,
+	
+	-- noice: Highly experimental plugin that replaces UI components
+	-- https://github.com/folke/noice.nvim
+	noice = false,
+	
+	-- null_ls: Use Neovim as a language server to inject LSP diagnostics, code actions, and more
+	-- https://github.com/jose-elias-alvarez/null-ls.nvim
+	null_ls = false,
+	
+	-- autopairs: Autopairs for Neovim
+	-- https://github.com/windwp/nvim-autopairs
+	autopairs = false,
+	
+	-- cmp: Completion engine for Neovim
+	-- https://github.com/hrsh7th/nvim-cmp
+	cmp = false,
+	
+	-- colorizer: Color highlighter for Neovim
+	-- https://github.com/norcalli/nvim-colorizer.lua
+	colorizer = false,
+	
+	-- dap: Debug Adapter Protocol client implementation for Neovim
+	-- https://github.com/mfussenegger/nvim-dap
+	dap = false,
+	
+	-- notify: Fancy, configurable notification manager for Neovim
+	-- https://github.com/rcarriga/nvim-notify
+	notify = false,
+	
+	-- surround: Surround selections, stylishly
+	-- https://github.com/kylechui/nvim-surround
+	surround = false,
+	
+	-- treesitter: Nvim Treesitter configurations and abstraction layer
+	-- https://github.com/nvim-treesitter/nvim-treesitter
+	treesitter = false,
+	
+	-- ufo: Folding powered by lsp, treesitter and more
+	-- https://github.com/kevinhwang91/nvim-ufo
+	ufo = false,
+	
+	-- onedark: One Dark theme for Neovim
+	-- https://github.com/navarasu/onedark.nvim
+	onedark = false,
+	
+	-- project: Project management for Neovim
+	-- https://github.com/ahmedkhalf/project.nvim
+	project = false,
+	
+	-- rainbow: Rainbow parentheses for Neovim
+	-- https://github.com/p00f/nvim-ts-rainbow
+	rainbow = false,
+	
+	-- scope: Visualize and search Treesitter scopes
+	-- https://github.com/tiagovla/scope.nvim
+	scope = false,
+	
+	-- telescope: Highly extendable fuzzy finder over lists
+	-- https://github.com/nvim-telescope/telescope.nvim
+	telescope = false,
+	
+	-- toggleterm: Persist and toggle multiple terminals
+	-- https://github.com/akinsho/toggleterm.nvim
+	toggleterm = false,
+	
+	-- trouble: Pretty diagnostics, references, telescope results, quickfix and location list
+	-- https://github.com/folke/trouble.nvim
+	trouble = false,
+	
+	-- twilight: Dim inactive portions of the code you're editing
+	-- https://github.com/folke/twilight.nvim
+	twilight = false,
+	
+	-- whichkey: Displays a popup with possible keybindings of the command you started typing
+	-- https://github.com/folke/which-key.nvim
+	whichkey = false,
+	
+	-- windline: Animations for Neovim's statusline
+	-- https://github.com/windwp/windline.nvim
+	windline = false,
+	
+	-- zen: Distraction-free coding for Neovim
+	-- https://github.com/folke/zen-mode.nvim
+	zen = false,
 }
 
 -- add extra plugins in here
